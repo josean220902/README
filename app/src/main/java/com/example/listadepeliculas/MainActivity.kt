ba40780fbdce9a2d92dad058b47e41c3784c3f55
JOSE LUIS <joseluismartinezsanchez48@gmail.com>
2021-11-08 19:36:53 +0100

    Primera version del proyecto

--- app/src/main/java/com/example/listadepeliculas/MainActivity.kt
+++ app/src/main/java/com/example/listadepeliculas/MainActivity.kt
@@ -8,11 +8,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("joseluis", "onCreate")
+        Log.d("joseluis", "La actividad se ha creado")
     }
-
+    override fun onStart() {
+        super.onStart()
+        Log.d("joseluis","Pasa a estado visible")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d("joseluis","Sale de estado visible")
+    }
     override fun onResume() {
         super.onResume()
-        Log.d("joseluis", "onResume")
+        Log.d("joseluis", "Empieza el estado foreground")
     }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("joseluis","la aplicacion se ha parado")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("joseluis","La actividad se ha destruido")
+    }
+
+
+
+
 }
