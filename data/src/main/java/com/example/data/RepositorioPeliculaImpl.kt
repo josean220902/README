4552bf0c200c7e7181f34443dcb3de5591602430
josean220902 <92751355+josean220902@users.noreply.github.com>
2022-02-14 19:43:50 +0100

    Ejercicio 12.5

--- data/src/main/java/com/example/data/RepositorioPeliculaImpl.kt
+++ data/src/main/java/com/example/data/RepositorioPeliculaImpl.kt
@@ -3,23 +3,31 @@ package com.example.data
 import com.example.domain.Pelicula
 import es.spain.data.ServerDataSource
 import repositorio.RepositorioPelicula
+import java.lang.Exception
 import javax.inject.Inject
 
 class RepositorioPeliculaImpl @Inject constructor(
-    private val serverDataSource: ServerDataSource
+    private val serverDataSource: ServerDataSource,
+    private val databaseDataSource: DatabaseDatasource
 ) : RepositorioPelicula {
     override suspend fun getPelicula(id: Int, language: String): Pelicula? {
-        return runCatching {
-            serverDataSource.getFilm(id, language)
-        }.getOrNull()
+        return try{
+            val filmFromServer = serverDataSource.getFilm(id,language)
+            databaseDataSource.updateFilms(listOf(filmFromServer))
+            filmFromServer
+        }catch (exception: Exception){
+            databaseDataSource.getFilm(id)
+        }
     }
 
     override suspend fun getFilms(language: String): List<Pelicula>? {
-        return runCatching {
-            serverDataSource.getFilms(language)
-        }.onFailure {
-            it
-        }.getOrNull()
+       return try{
+           val filmsFromServer = serverDataSource.getFilms(language)
+           databaseDataSource.updateFilms(filmsFromServer)
+           filmsFromServer
+       }catch (exception: Exception){
+           databaseDataSource.getFilms()
+       }
     }
 
 }
